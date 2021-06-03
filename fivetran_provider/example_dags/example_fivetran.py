259ccad7475b037b1140cb48717067e15eeabcff
Josh Fell <josh.d.fell@astronomer.io>
2021-06-03 11:00:59 -0400

    Updating example DAGs for best practices
    - Modified the `start_date` to have a static value
    - Added `catchup=False` expression to the DAG definition
    - Using the `DAG` object as a context manager
    - Added an explicit `schedule_interval` (the default value from `airflow.models.DAG`)
    - Removed unused modules from import statements

--- fivetran_provider/example_dags/example_fivetran.py
+++ fivetran_provider/example_dags/example_fivetran.py
@@ -1,33 +1,35 @@
-import airflow
 from airflow import DAG
-from airflow.models import Variable
+
 from fivetran_provider.operators.fivetran import FivetranOperator
 from fivetran_provider.sensors.fivetran import FivetranSensor
+
+from datetime import datetime, timedelta
 
 
 default_args = {
     "owner": "Airflow",
-    "start_date": airflow.utils.dates.days_ago(1)
+    "start_date": datetime(2021, 4, 6),
 }
 
 dag = DAG(
     dag_id='example_fivetran',
-    default_args=default_args
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    catchup=False,
 )
 
-fivetran_sync_start = FivetranOperator(
-    task_id='fivetran-task',
-    fivetran_conn_id='fivetran_default',
-    connector_id="{{ var.value.get('connector_id') }}",
-    dag=dag
-)
+with dag:
+    fivetran_sync_start = FivetranOperator(
+        task_id='fivetran-task',
+        fivetran_conn_id='fivetran_default',
+        connector_id="{{ var.value.connector_id }}",
+    )
 
-fivetran_sync_wait = FivetranSensor(
-    task_id='fivetran-sensor',
-    fivetran_conn_id='fivetran_default',
-    connector_id="{{ var.value.get('connector_id') }}",
-    poke_interval=5,
-    dag=dag
-)
+    fivetran_sync_wait = FivetranSensor(
+        task_id='fivetran-sensor',
+        fivetran_conn_id='fivetran_default',
+        connector_id="{{ var.value.connector_id }}",
+        poke_interval=5,
+    )
 
-fivetran_sync_start >> fivetran_sync_wait
+    fivetran_sync_start >> fivetran_sync_wait
